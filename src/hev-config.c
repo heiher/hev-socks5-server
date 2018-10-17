dacfca9f454002f5cd73eaa94b7141e4d418ed13
Heiher <r@hev.cc>
2018-10-17 22:15:48 +0800

    HevConfig: Sync to new coding style.

--- src/hev-config.c
+++ src/hev-config.c
@@ -21,50 +21,50 @@ static unsigned int workers;
 int
 hev_config_init (const char *config_path)
 {
-	dictionary *ini_dict;
+    dictionary *ini_dict;
 
-	ini_dict = iniparser_load (config_path);
-	if (!ini_dict) {
-		fprintf (stderr, "Load config from file %s failed!\n",
-					config_path);
-		return -1;
-	}
+    ini_dict = iniparser_load (config_path);
+    if (!ini_dict) {
+        fprintf (stderr, "Load config from file %s failed!\n", config_path);
+        return -1;
+    }
 
-	/* Main:ListenAddress */
-	char *address = iniparser_getstring (ini_dict, "Main:ListenAddress", NULL);
-	if (!address) {
-		fprintf (stderr, "Get Main:ListenAddress from file %s failed!\n", config_path);
-		iniparser_freedict (ini_dict);
-		return -2;
-	}
-	strncpy (listen_address, address, 16);
+    /* Main:ListenAddress */
+    char *address = iniparser_getstring (ini_dict, "Main:ListenAddress", NULL);
+    if (!address) {
+        fprintf (stderr, "Get Main:ListenAddress from file %s failed!\n",
+                 config_path);
+        iniparser_freedict (ini_dict);
+        return -2;
+    }
+    strncpy (listen_address, address, 16);
 
-	/* Main:Port */
-	port = iniparser_getint (ini_dict, "Main:Port", -1);
-	if (-1 == port) {
-		fprintf (stderr, "Get Main:Port from file %s failed!\n",
-					config_path);
-		iniparser_freedict (ini_dict);
-		return -3;
-	}
+    /* Main:Port */
+    port = iniparser_getint (ini_dict, "Main:Port", -1);
+    if (-1 == port) {
+        fprintf (stderr, "Get Main:Port from file %s failed!\n", config_path);
+        iniparser_freedict (ini_dict);
+        return -3;
+    }
 
-	/* Main:DNSAddress */
-	address = iniparser_getstring (ini_dict, "Main:DNSAddress", NULL);
-	if (!address) {
-		fprintf (stderr, "Get Main:DNSAddress from file %s failed!\n", config_path);
-		iniparser_freedict (ini_dict);
-		return -4;
-	}
-	strncpy (dns_address, address, 16);
+    /* Main:DNSAddress */
+    address = iniparser_getstring (ini_dict, "Main:DNSAddress", NULL);
+    if (!address) {
+        fprintf (stderr, "Get Main:DNSAddress from file %s failed!\n",
+                 config_path);
+        iniparser_freedict (ini_dict);
+        return -4;
+    }
+    strncpy (dns_address, address, 16);
 
-	/* Main:Workers */
-	workers = iniparser_getint (ini_dict, "Main:Workers", 1);
-	if (workers <= 0)
-		workers = 1;
+    /* Main:Workers */
+    workers = iniparser_getint (ini_dict, "Main:Workers", 1);
+    if (workers <= 0)
+        workers = 1;
 
-	iniparser_freedict (ini_dict);
+    iniparser_freedict (ini_dict);
 
-	return 0;
+    return 0;
 }
 
 void
@@ -75,24 +75,23 @@ hev_config_fini (void)
 unsigned int
 hev_config_get_workers (void)
 {
-	return workers;
+    return workers;
 }
 
 const char *
 hev_config_get_listen_address (void)
 {
-	return listen_address;
+    return listen_address;
 }
 
 unsigned short
 hev_config_get_port (void)
 {
-	return port;
+    return port;
 }
 
 const char *
 hev_config_get_dns_address (void)
 {
-	return dns_address;
+    return dns_address;
 }
-
--- src/hev-config.h
+++ src/hev-config.h
@@ -15,9 +15,8 @@ void hev_config_fini (void);
 
 unsigned int hev_config_get_workers (void);
 
-const char * hev_config_get_listen_address (void);
+const char *hev_config_get_listen_address (void);
 unsigned short hev_config_get_port (void);
-const char * hev_config_get_dns_address (void);
+const char *hev_config_get_dns_address (void);
 
 #endif /* __HEV_CONFIG_H__ */
-
