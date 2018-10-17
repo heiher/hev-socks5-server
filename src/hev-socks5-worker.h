7e4a6f2d156ca31fa178df98bc20d86f4c6c5939
Heiher <r@hev.cc>
2018-10-17 22:16:44 +0800

    HevSocks5Worker: Sync to new coding style.

--- src/hev-socks5-worker.h
+++ src/hev-socks5-worker.h
@@ -12,11 +12,10 @@
 
 typedef struct _HevSocks5Worker HevSocks5Worker;
 
-HevSocks5Worker * hev_socks5_worker_new (int fd);
+HevSocks5Worker *hev_socks5_worker_new (int fd);
 void hev_socks5_worker_destroy (HevSocks5Worker *self);
 
 void hev_socks5_worker_start (HevSocks5Worker *self);
 void hev_socks5_worker_stop (HevSocks5Worker *self);
 
 #endif /* __HEV_SOCKS5_WORKER_H__ */
-
