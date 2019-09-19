502447141f3ca028fb56d2cc0c2a612ad7dcdf94
Heiher <r@hev.cc>
2019-09-19 10:43:53 +0800

    HevSocks5Worker: Use logger.

--- src/hev-socks5-worker.c
+++ src/hev-socks5-worker.c
@@ -19,6 +19,7 @@
 #include "hev-socks5-session.h"
 #include "hev-memory-allocator.h"
 #include "hev-config.h"
+#include "hev-logger.h"
 #include "hev-task.h"
 #include "hev-task-io.h"
 #include "hev-task-io-pipe.h"
@@ -55,7 +56,7 @@ hev_socks5_worker_new (int fd)
 
     self = hev_malloc0 (sizeof (HevSocks5Worker));
     if (!self) {
-        fprintf (stderr, "Allocate worker failed!\n");
+        LOG_E ("Allocate worker failed!");
         goto exit;
     }
 
@@ -65,19 +66,19 @@ hev_socks5_worker_new (int fd)
 
     self->task_worker = hev_task_new (8192);
     if (!self->task_worker) {
-        fprintf (stderr, "Create worker's task failed!\n");
+        LOG_E ("Create worker's task failed!");
         goto exit_free;
     }
 
     self->task_event = hev_task_new (8192);
     if (!self->task_event) {
-        fprintf (stderr, "Create event's task failed!\n");
+        LOG_E ("Create event's task failed!");
         goto exit_free_task_worker;
     }
 
     self->task_session_manager = hev_task_new (8192);
     if (!self->task_session_manager) {
-        fprintf (stderr, "Create session manager's task failed!\n");
+        LOG_E ("Create session manager's task failed!");
         goto exit_free_task_event;
     }
 
@@ -117,7 +118,7 @@ hev_socks5_worker_stop (HevSocks5Worker *self)
         return;
 
     if (write (self->event_fds[1], &val, sizeof (val)) == -1)
-        fprintf (stderr, "Write stop event failed!\n");
+        LOG_E ("Write stop event failed!");
 }
 
 static int
@@ -148,25 +149,11 @@ hev_socks5_worker_task_entry (void *data)
         client_fd = hev_task_io_socket_accept (self->fd, addr, &addr_len,
                                                worker_task_io_yielder, self);
         if (-1 == client_fd) {
-            fprintf (stderr, "Accept failed!\n");
+            LOG_E ("Accept failed!");
             continue;
         } else if (-2 == client_fd) {
             break;
         }
-
-#ifdef _DEBUG
-        {
-            char buf[64];
-            const char *sa = NULL;
-            uint16_t port = 0;
-            if (sizeof (addr6) == addr_len) {
-                sa = inet_ntop (AF_INET6, &addr6.sin6_addr, buf, sizeof (buf));
-                port = ntohs (addr6.sin6_port);
-            }
-            printf ("Worker %p: New client %d enter from [%s]:%u\n", self,
-                    client_fd, sa, port);
-        }
-#endif
 
         session =
             hev_socks5_session_new (client_fd, session_close_handler, self);
@@ -189,7 +176,7 @@ hev_socks5_event_task_entry (void *data)
     int val;
 
     if (-1 == hev_task_io_pipe_pipe (self->event_fds)) {
-        fprintf (stderr, "Create eventfd failed!\n");
+        LOG_E ("Create eventfd failed!");
         return;
     }
 
@@ -204,21 +191,11 @@ hev_socks5_event_task_entry (void *data)
     hev_task_wakeup (self->task_session_manager);
 
     /* wakeup sessions's task */
-#ifdef _DEBUG
-    printf ("Worker %p: Enumerating session list ...\n", self);
-#endif
     for (session = self->session_list; session; session = session->next) {
-#ifdef _DEBUG
-        printf ("Worker %p: Set session %p's hp = 0\n", self, session);
-#endif
         session->hp = 0;
 
         /* wakeup session's task to do destroy */
         hev_task_wakeup (session->task);
-#ifdef _DEBUG
-        printf ("Worker %p: Wakeup session %p's task %p\n", self, session,
-                session->task);
-#endif
     }
 
     close (self->event_fds[0]);
@@ -237,24 +214,13 @@ hev_socks5_session_manager_task_entry (void *data)
         if (self->quit)
             break;
 
-#ifdef _DEBUG
-        printf ("Worker %p: Enumerating session list ...\n", self);
-#endif
         for (session = self->session_list; session; session = session->next) {
-#ifdef _DEBUG
-            printf ("Worker %p: Session %p's hp %d\n", self, session,
-                    session->hp);
-#endif
             session->hp--;
             if (session->hp > 0)
                 continue;
 
             /* wakeup session's task to do destroy */
             hev_task_wakeup (session->task);
-#ifdef _DEBUG
-            printf ("Worker %p: Wakeup session %p's task %p\n", self, session,
-                    session->task);
-#endif
         }
     }
 }
@@ -265,9 +231,6 @@ session_manager_insert_session (HevSocks5Worker *self,
 {
     HevSocks5SessionBase *session_base = (HevSocks5SessionBase *)session;
 
-#ifdef _DEBUG
-    printf ("Worker %p: Insert session: %p\n", self, session);
-#endif
     /* insert session to session_list */
     session_base->prev = NULL;
     session_base->next = self->session_list;
@@ -282,9 +245,6 @@ session_manager_remove_session (HevSocks5Worker *self,
 {
     HevSocks5SessionBase *session_base = (HevSocks5SessionBase *)session;
 
-#ifdef _DEBUG
-    printf ("Worker %p: Remove session: %p\n", self, session);
-#endif
     /* remove session from session_list */
     if (session_base->prev) {
         session_base->prev->next = session_base->next;
