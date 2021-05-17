352860383160f80f8b6cee26650ae2b9094096cf
hev <r@hev.cc>
2021-05-17 22:58:56 +0800

    HevList: Init.

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2021 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -30,6 +30,8 @@ include $(LOCAL_PATH)/build.mk
 LOCAL_MODULE    := hev-socks5-server
 LOCAL_SRC_FILES := $(patsubst $(SRCDIR)/%,src/%,$(SRCFILES))
 LOCAL_C_INCLUDES := \
+	$(LOCAL_PATH)/src/misc \
+	$(LOCAL_PATH)/src/core/include \
 	$(LOCAL_PATH)/third-part/yaml/src \
 	$(LOCAL_PATH)/third-part/hev-task-system/include
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
