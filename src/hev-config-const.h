58db7a70dd31e2236f23e9db5056a25a7c6b895f
hev <r@hev.cc>
2023-02-23 21:30:39 +0800

    HevConfig: Bump up to 2.5.0.

--- src/hev-config-const.h
+++ src/hev-config-const.h
@@ -2,7 +2,7 @@
  ============================================================================
  Name        : hev-config-const.h
  Author      : Heiher <r@hev.cc>
- Copyright   : Copyright (c) 2017 - 2022 hev
+ Copyright   : Copyright (c) 2017 - 2023 hev
  Description : Config Constants
  ============================================================================
  */
@@ -11,7 +11,7 @@
 #define __HEV_CONFIG_CONST_H__
 
 #define MAJOR_VERSION (2)
-#define MINOR_VERSION (4)
-#define MICRO_VERSION (7)
+#define MINOR_VERSION (5)
+#define MICRO_VERSION (0)
 
 #endif /* __HEV_CONFIG_CONST_H__ */
